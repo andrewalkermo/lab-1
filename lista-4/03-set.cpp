6c4a6650acdedda16ae81f6f009b458c7d769ad2
André Walker <andrewalker@infojr.com.br>
2017-07-17 11:24:29 -0300

    .

---
+++ lista-4/03-set.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <set>
+using namespace std;
+int main() {
+    int n;
+    while (cin >> n && !(n == 0)) {
+        string fentico;
+        cin >> fentico;
+        set<int> total;
+        for (int i = 0; i < fentico.length(); i++) {
+            total.insert(id);
+        }
+    }
+}
